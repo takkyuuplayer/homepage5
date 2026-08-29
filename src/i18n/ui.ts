51d830528c8dbc0fb30c9fa0f208dfb9fc7fceac
takkyuuplayer <takkyuuplayer@gmail.com>
2026-08-29 11:34:28 +0900

    format

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,16 +1,15 @@
 export const defaultLang = "ja";
 
-
 export const ui = {
-    ja: {
-        "site.title": "卓球Playerの遊び場",
-        "logo.alt": "ロゴ"
-    },
-    en: {
-        "site.title": "takkyuuplayer's playground",
-        "logo.alt": "Logo"
-    }
+	ja: {
+		"site.title": "卓球Playerの遊び場",
+		"logo.alt": "ロゴ",
+	},
+	en: {
+		"site.title": "takkyuuplayer's playground",
+		"logo.alt": "Logo",
+	},
 } as const;
 
 export type Lang = keyof typeof ui;
-export type UIKey  = keyof (typeof ui)[typeof defaultLang];
+export type UIKey = keyof (typeof ui)[typeof defaultLang];
